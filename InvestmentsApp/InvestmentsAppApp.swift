e4835dc707caeb44a8468afd3c0ecad795163abc
Litvak Lev <sn.delui@gmail.com>
2022-06-06 18:02:14 +0500

    Initial Commit

---
+++ InvestmentsApp/InvestmentsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentsAppApp.swift
+//  InvestmentsApp
+//
+//  Created by Lev Litvak on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
